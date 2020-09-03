f9a67594ea7915e556a0a137e0cda97cc7d6a72e
unknown <16841475+demanbart@users.noreply.github.com>
2020-09-03 18:26:08 +0200

    foo

--- pibot/motor/microArduinoI2CServoController.py
+++ pibot/motor/microArduinoI2CServoController.py
@@ -4,9 +4,9 @@ class CameraMount:
   def __init__(self):
     self.MAXVALUE = 180
     self.MINVALUE = 0
-    DEVICE_BUS = 1
+    self.DEVICE_BUS = 1
     self.DEVICE_ADDR = 0x04
-    self.bus = smbus.SMBus(DEVICE_BUS)
+    self.bus = smbus.SMBus(self.DEVICE_BUS)
 
   def execute(self, command):
     #this is how to send the position to the arduino, see /doc/cameramount for arduinocode
